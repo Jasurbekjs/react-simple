472a47180be718249b53ab804681bac68d39cb19
Jasurbek <jasmirzaev@gmail.com>
2023-08-23 21:53:12 +0300

    add styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,32 @@
+import styles from './Home.module.css'
+
 const Home = () => {
     return (
         <>
           <div>
             <h1>Cars Catalog</h1>  
-            <div>
-                <h2>Car 1</h2>
-                <p>$100 000</p>
-                <button>Read more</button>
+            <div
+                /* inline styles - bad practice */ 
+                // style={{
+                //     border: '1px solid red' 
+                // }} 
+                /**/
+
+                /** ClassName - best practice */
+                className={styles.item}
+            >
+                <div
+                    className={styles.image}
+                    style={{
+                        backgroundImage: 'url(/camry.jpg)'
+                    }}
+                />
+                {/* <img src='/camry.jpg' alt='camry'/> */}
+                <div className={styles.info}>
+                    <h2>Car 1</h2>
+                    <p>$100 000</p>
+                    <button>Read more</button>
+                </div>
             </div>
           </div>
         </>
