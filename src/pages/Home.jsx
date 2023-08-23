3a57df9d9e2ce1cdb6129a9017b0e0c0bb87ca2d
Jasurbek <jasmirzaev@gmail.com>
2023-08-23 20:28:14 +0300

    clear & Home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+        <>
+          <div>
+            <h1>Cars Catalog</h1>  
+            <div>
+                <h2>Car 1</h2>
+                <p>$100 000</p>
+                <button>Read more</button>
+            </div>
+          </div>
+        </>
+    )
+}
+
+export default Home
