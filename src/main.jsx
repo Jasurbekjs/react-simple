d8319e75df712026f8804e4e7a68e2baa057d03f
Jasurbek <jasmirzaev@gmail.com>
2023-08-31 04:19:59 +0300

    useContext/addGlobal

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Router from './router/Router.jsx'
+import AuthProvider from './providers/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
   </React.StrictMode>,
 )
